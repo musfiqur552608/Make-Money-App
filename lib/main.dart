4271085081a694491370220d774c33a31603eddd
Musfiqur Rahman <musfiqurrahmansayed@gmail.com>
2025-03-01 00:38:34 +0600

    Add main screen layout with bank balance and button

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -10,14 +10,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      theme: ThemeData.dark(useMaterial3: true),
       home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('Make Money App'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
+        appBar: AppBar(centerTitle: true, title: Text('Make Money App')),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          color: Colors.blueGrey[700],
+          height: double.infinity,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                flex: 9,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Text("Bank Balance"),
+                    SizedBox(height: 20),
+                    Text("\$ 1000")],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red[700],
+                    minimumSize: Size(double.infinity, 0),
+                  ),
+                  onPressed: () {
+                    print("Button Clicked");
+                  },
+                  child: Text("Click Here"),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
